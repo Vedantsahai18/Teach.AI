f2d14d1dedfc20c1e44f414cfacedf74562c1f8b
Vedant Sahai <vedantsahai18@gmail.com>
2020-01-10 16:25:43 +0530

    India-Singapore Hackathon

---
+++ frontend-circles/circles/build/precache-manifest.226665c4644cd39bd231c2c1e9af7cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9426e96e1d07fb8d1aca91f5f5781b37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7fd06b31605aa709084",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b635bdd6fc8e847d0c8a",
+    "url": "/static/js/2.faf93e9b.chunk.js"
+  },
+  {
+    "revision": "c3a0f043cc1cbf31ed3ead101c80e05a",
+    "url": "/static/js/2.faf93e9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7fd06b31605aa709084",
+    "url": "/static/js/main.f0b16df4.chunk.js"
+  },
+  {
+    "revision": "9a412c72d6abb45d8a77",
+    "url": "/static/js/runtime-main.61c477b1.js"
+  }
+]);
---
+++ frontend-dashboard/Teachers-Pet-React/build/precache-manifest.d7ae935d2129f00edc133097ce7278e7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6a459664b0715a4fb15828d3bd860e",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "d7729dcb904bb1fa9b6e",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "a0097935710739704801",
+    "url": "/material-dashboard-react/static/css/main.6eb76ea9.chunk.css"
+  },
+  {
+    "revision": "d7729dcb904bb1fa9b6e",
+    "url": "/material-dashboard-react/static/js/2.512f416f.chunk.js"
+  },
+  {
+    "revision": "69aec335a1082d28781c1550b1770e6e",
+    "url": "/material-dashboard-react/static/js/2.512f416f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0097935710739704801",
+    "url": "/material-dashboard-react/static/js/main.93ffc2d6.chunk.js"
+  },
+  {
+    "revision": "f93bb5dd34b88cb79046",
+    "url": "/material-dashboard-react/static/js/runtime-main.29fe7105.js"
+  },
+  {
+    "revision": "1773df5a11a6b639fb6a1f4ee1884864",
+    "url": "/material-dashboard-react/static/media/Gabriel_Sze_Compressed.1773df5a.jpg"
+  },
+  {
+    "revision": "b5a4c1ef1fa3f55e2599583d196ee82d",
+    "url": "/material-dashboard-react/static/media/Halftime1.b5a4c1ef.png"
+  },
+  {
+    "revision": "cafe668e015bdbf4da4f5ac130711b7b",
+    "url": "/material-dashboard-react/static/media/TeachingPed.cafe668e.png"
+  },
+  {
+    "revision": "952c454ce7cc7ba806c174d4e1df9863",
+    "url": "/material-dashboard-react/static/media/junjia.952c454c.jpg"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "4c51de350344d1fd88503a9cf9a1a562",
+    "url": "/material-dashboard-react/static/media/reactlogo.4c51de35.png"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "9ea3195f85df9a5a2979166c6d35a362",
+    "url": "/material-dashboard-react/static/media/students.9ea3195f.png"
+  }
+]);
