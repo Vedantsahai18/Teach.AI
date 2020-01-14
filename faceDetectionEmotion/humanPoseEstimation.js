5d2311966a31adc778d6ba1705c6a4a3f128b29b
Vedant Sahai <vedantsahai18@gmail.com>
2020-01-14 18:33:02 +0530

    2 models merged

--- faceDetectionEmotion/humanPoseEstimation.js
+++ faceDetectionEmotion/humanPoseEstimation.js
@@ -23,35 +23,6 @@ function HumanPoseEstimationSetup(video) {
   return poseNet;
 }
 
-// function draw() {
-//   image(video, 0, 0, width, height);
-//   drawKeypoints();
-//   drawSkeleton();
-// }
-
-// function drawKeypoints()  {
-//   for (let i = 0; i < poses.length; i++) {
-//     for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
-//       let keypoint = poses[i].pose.keypoints[j];
-//       if (keypoint.score > 0.2) {
-//         fill(255, 0, 0);
-//         noStroke();
-//         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
-//       }
-//     }
-//   }
-// }
-
-// function drawSkeleton() {
-//   for (let i = 0; i < poses.length; i++) {
-//     for (let j = 0; j < poses[i].skeleton.length; j++) {
-//       let partA = poses[i].skeleton[j][0];
-//       let partB = poses[i].skeleton[j][1];
-//       stroke(255, 0, 0);
-//       line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
-//     }
-//   }
-// }
 var abc = ["raisHand", "sleeping", "eyeCoordX", "eyeCoordY", "timestamp"]
 var xyz = []
 abc = abc + "\n"
